714b8265968d226ab6db980e06dba01612a48616
Markus Barthlen <your_email@example.com>
2016-09-18 15:50:08 +0200

    WIP

--- src/Main.purs
+++ src/Main.purs
@@ -5,10 +5,17 @@ import React as R
 import React.DOM.Props as RP
 import ReactDOM as RDOM
 import Thermite as T
+import Control.Monad.Aff (liftEff')
+import Control.Monad.Aff.Class (liftAff)
 import Control.Monad.Eff (Eff)
+import Control.Monad.Eff.Class (liftEff)
+import Control.Monad.Eff.Console (log, CONSOLE)
 import Control.Monad.Eff.Exception (try)
+import Control.Monad.Trans (lift)
 import DOM (DOM)
 import DOM.HTML (window) as DOM
+import DOM.HTML.HTMLAnchorElement (download)
+import DOM.HTML.HTMLInputElement (files)
 import DOM.HTML.Types (htmlDocumentToParentNode) as DOM
 import DOM.HTML.Window (document) as DOM
 import DOM.Node.ParentNode (querySelector) as DOM
@@ -18,26 +25,19 @@ import Data.Nullable (toMaybe)
 import Node.FS (FS)
 import Node.FS.Sync (readdir)
 import Partial.Unsafe (unsafePartial)
-import React.DOM (text, li', ul', input, div)
+import React.DOM (text, li', ul', input)
 import Unsafe.Coerce (unsafeCoerce)
-import Control.Monad.Trans (lift)
-import Control.Monad.Eff.Class (liftEff)
-import Control.Monad.Eff.Console (log, CONSOLE)
-import Data.Lens (over)
-import Data.Foldable (fold)
-
-
 
 type State = {dir :: String, names :: Array String}
 
 data Action
   = SetEditText String |
-  UpdateFiles String
+  UpdateFiles String | Update
 
 render :: T.Render State _ Action
 render perform props state _ =
   let handleKeyPress :: Int -> String -> _
-      handleKeyPress 13 s = perform $ UpdateFiles s
+      handleKeyPress 13 text = perform $ UpdateFiles text
       handleKeyPress 27 _    = perform $ SetEditText ""
       handleKeyPress _  _    = pure unit
   in
@@ -47,50 +47,31 @@ render perform props state _ =
                RP.onChange \e -> perform (SetEditText (unsafeCoerce e).target.value),
                RP.onKeyUp \e -> handleKeyPress (unsafeCoerce e).keyCode (unsafeCoerce e).target.value
                ] [],
-      ul' (map (\file -> li' [text file]) props.names)
+      ul' (map (\file -> li' [text file, input [RP.value "test", RP.onClick (\_ -> performAction Update) ] []]) props.names)
     ]
 
 
 performAction :: forall e. T.PerformAction (fs :: FS, console :: CONSOLE | e) State _ Action
 performAction (SetEditText s)           _ _ = void do
   T.cotransform $ _ { dir = s }
-performAction _                     _ _ = pure unit
-
-
--- dirListingComponent :: T.Spec _ State _ Action
-dirListingComponent :: forall e. T.Spec (fs :: FS, console :: CONSOLE | e) State _ Action
-dirListingComponent = T.simpleSpec performAction render
-
-performActionParent :: forall e. T.PerformAction (fs :: FS, console :: CONSOLE | e) State _ Action
-performActionParent (UpdateFiles s)           _ _ = do
+performAction (Update)                  _ _ = void do
+  T.cotransform $ _ { dir = "" }
+performAction (UpdateFiles s)           _ _ = do
    filenames <- lift ( liftEff (either (const []) id <$> try (readdir s)))
    lift (liftEff $ log s)
    lift (liftEff $ log (show filenames))
    void $ T.cotransform $ _ { names = filenames }
-performActionParent _                     _ _ = pure unit
 
--- renderParent :: forall props e. T.Render State _ Action
--- renderParent dispatch props state _ = container $ fold
-parent :: forall eff. T.Spec (fs :: FS, console :: CONSOLE | eff) State _ Action
-parent = container $ fold
-  [  dirListingComponent , parentActions
-    ]
-  where
-  container :: forall state action. T.Spec (fs :: FS, console :: CONSOLE | eff) state _ action -> T.Spec (fs :: FS, console :: CONSOLE | eff) state _ action
-  container = over T._render \renderX d p s c ->
-      [ div [ RP.className "container" ] (renderX d p s c) ]
-  parentActions :: T.Spec (fs :: FS, console :: CONSOLE | eff) State _ Action
-  parentActions = T.simpleSpec performActionParent T.defaultRender
 
--- parent :: T.Spec _ State _ Action
--- parent = T.simpleSpec performActionParent renderParent
+dirListingComponent :: T.Spec _ State _ Action
+dirListingComponent = T.simpleSpec performAction render
 
 main :: Eff (fs :: FS, dom :: DOM) Unit
 main = void do
   let dir = "/home/markus"
   fileNames <- either (const []) id <$> try (readdir dir)
   let state = {dir: dir, names: fileNames}
-  let component = T.createClass parent state
+  let component = T.createClass dirListingComponent state
   document <- DOM.window >>= DOM.document
   container <-
     unsafePartial
