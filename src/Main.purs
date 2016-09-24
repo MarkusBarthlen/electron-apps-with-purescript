375e33c04da02aa7b5832170ce37b2c3acf2a54a
Markus Barthlen <your_email@example.com>
2016-09-24 07:42:25 +0200

    WIP

--- src/Main.purs
+++ src/Main.purs
@@ -29,24 +29,16 @@ import React.DOM (text, li', ul', input, button)
 import Unsafe.Coerce (unsafeCoerce)
 import Data.Int (toNumber)
 
-type State = {a :: Int, b :: Int, dir :: String, names :: Array String}
+type State = {a :: Int, b :: Int}
 
 data Action
-  = SetEditText String |
-  UpdateFiles String | Update | IncrementA | IncrementB
+  =  IncrementA | IncrementB
 
 render :: T.Render State _ Action
 render perform props state _ =
-  let handleKeyPress :: Int -> String -> _
-      handleKeyPress 13 text = perform $ UpdateFiles text
-      handleKeyPress 27 _    = perform $ SetEditText ""
-      handleKeyPress _  _    = pure unit
-  in
     [
-
           text (show (toNumber props.a))
          ,
-
       button [ RP.className "btn btn-success"
                    , RP.onClick \_ -> perform IncrementB
                    ]
@@ -59,30 +51,19 @@ render perform props state _ =
 
 
 performAction :: forall e. T.PerformAction (fs :: FS, console :: CONSOLE | e) State _ Action
-performAction IncrementA                _ _ = void $ T.cotransform $ \{a = x, b = y , dir = z, names = w} -> {a = x +1, b = y , dir = z, names = w}
+performAction IncrementA                _ _ = void do
+  T.cotransform \state -> state { a = 20}
 performAction IncrementB                _ _ = void do
-  T.cotransform $ _ { b = 20 }
-performAction (SetEditText s)           _ _ = void do
-  T.cotransform $ _ { dir = s }
-performAction (Update)                  _ _ = void do
-  T.cotransform $ _ { dir = "" }
-performAction (UpdateFiles s)           _ p = do
-   lift (liftEff $ log (show p.names))
-   filenames <- lift ( liftEff (either (const []) id <$> try (readdir s)))
-   lift (liftEff $ log s)
-   lift (liftEff $ log (show filenames))
-   void $ T.cotransform $ _ { names = filenames }
+  T.cotransform $ _ {b = 20 }
 
 
-dirListingComponent :: T.Spec _ State _ Action
-dirListingComponent = T.simpleSpec performAction render
+komponent :: T.Spec _ State _ Action
+komponent = T.simpleSpec performAction render
 
 main :: Eff (fs :: FS, dom :: DOM) Unit
 main = void do
-  let dir = "/home/markus"
-  fileNames <- either (const []) id <$> try (readdir dir)
-  let state = {dir: dir, names: fileNames, a: 0 , b :0}
-  let component = T.createClass dirListingComponent state
+  let state = {a: 0 , b :0}
+  let component = T.createClass komponent state
   document <- DOM.window >>= DOM.document
   container <-
     unsafePartial
