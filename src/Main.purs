7227c7c6c9fbd5f32ff6371203239f4017cee6ce
Markus Barthlen <your_email@example.com>
2016-09-25 03:14:28 +0200

    temp commit

--- src/Main.purs
+++ src/Main.purs
@@ -4,13 +4,12 @@ import Prelude
 import React as R
 import React.DOM.Props as RP
 import ReactDOM as RDOM
-import Thermite as T
 import Control.Monad.Aff (liftEff')
 import Control.Monad.Aff.Class (liftAff)
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Class (liftEff)
 import Control.Monad.Eff.Console (log, CONSOLE)
-import Control.Monad.Eff.Exception (try)
+import Control.Monad.Eff.Exception (try, EXCEPTION)
 import Control.Monad.Trans (lift)
 import DOM (DOM)
 import DOM.HTML (window) as DOM
@@ -29,47 +28,36 @@ import React.DOM (text, li', ul', input, button)
 import Unsafe.Coerce (unsafeCoerce)
 import Data.Int (toNumber)
 
-type State = {a :: Int, b :: Int}
+import Signal.Channel (CHANNEL)
 
-data Action
-  =  IncrementA | IncrementB
+import Pux (renderToDOM, fromSimple, start)
+import Pux.Html (Html, text, button, span, div)
+import Pux.Html.Events (onClick)
 
-render :: T.Render State _ Action
-render perform props state _ =
-    [
-          text (show (toNumber props.a))
-         ,
-      button [ RP.className "btn btn-success"
-                   , RP.onClick \_ -> perform IncrementB
-                   ]
-                   [ text (show (toNumber props.b))]
-        , button [ RP.className "btn btn-danger"
-                   , RP.onClick \_ -> perform IncrementA
-                   ]
-                   [ text (show (toNumber props.a)) ]
+data Action = Increment | Decrement
+
+type State = Int
+
+update :: Action -> State -> State
+update Increment count = count + 1
+update Decrement count = count - 1
+
+view :: State -> Html Action
+view count =
+  div
+    []
+    [ button [ onClick (const Increment) ] [ text "Increment" ]
+    , span [] [ text (show count) ]
+    , button [ onClick (const Decrement) ] [ text "Decrement" ]
     ]
 
+main :: forall e. Eff (err :: EXCEPTION, channel :: CHANNEL | e) Unit
+main = do
+  app <- start
+    { initialState: 0
+    , update: fromSimple update
+    , view: view
+    , inputs: []
+    }
 
-performAction :: forall e. T.PerformAction (fs :: FS, console :: CONSOLE | e) State _ Action
-performAction IncrementA                _ _ = void do
-  T.cotransform \state -> state { a = 20}
-performAction IncrementB                _ _ = void do
-  T.cotransform $ _ {b = 20 }
-
-
-komponent :: T.Spec _ State _ Action
-komponent = T.simpleSpec performAction render
-
-main :: Eff (fs :: FS, dom :: DOM) Unit
-main = void do
-  let state = {a: 0 , b :0}
-  let component = T.createClass komponent state
-  document <- DOM.window >>= DOM.document
-  container <-
-    unsafePartial
-    (fromJust <<< toMaybe
-    <$> DOM.querySelector "#container"
-    (DOM.htmlDocumentToParentNode document))
-  RDOM.render
-    (R.createFactory component (state))
-    container
+  renderToDOM "#app" app.html
