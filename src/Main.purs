128c00e055024c939861edd082167bfa8939b0ac
Markus Barthlen <your_email@example.com>
2016-09-25 12:11:07 +0200

    Merged

--- src/Main.purs
+++ src/Main.purs
@@ -1,15 +1,19 @@
 module Main where
 
-import Prelude
+
+
+{--
 import React as R
 import React.DOM.Props as RP
 import ReactDOM as RDOM
 import Control.Monad.Aff (liftEff')
 import Control.Monad.Aff.Class (liftAff)
-import Control.Monad.Eff (Eff)
+
 import Control.Monad.Eff.Class (liftEff)
 import Control.Monad.Eff.Console (log, CONSOLE)
-import Control.Monad.Eff.Exception (try, EXCEPTION)
+
+
+
 import Control.Monad.Trans (lift)
 import DOM (DOM)
 import DOM.HTML (window) as DOM
@@ -24,9 +28,17 @@ import Data.Nullable (toMaybe)
 import Node.FS (FS)
 import Node.FS.Sync (readdir)
 import Partial.Unsafe (unsafePartial)
-import React.DOM (text, li', ul', input, button)
+-- import React.DOM
 import Unsafe.Coerce (unsafeCoerce)
 import Data.Int (toNumber)
+
+
+--}
+
+import Prelude
+
+import Control.Monad.Eff.Exception (try, EXCEPTION)
+import Control.Monad.Eff (Eff)
 
 import Signal.Channel (CHANNEL)
 
@@ -34,30 +46,37 @@ import Pux (renderToDOM, fromSimple, start)
 import Pux.Html (Html, text, button, span, div)
 import Pux.Html.Events (onClick)
 
-data Action = Increment | Decrement
+import Data.Int (toNumber)
 
-type State = Int
+
+data Action = IncrementA | IncrementB
+
+type State = {a :: Int, b :: Int}
+
 
 update :: Action -> State -> State
-update Increment count = count + 1
-update Decrement count = count - 1
+--update IncrementA state = {a = 20, b = 20}
+update IncrementA state = state { a = state.a + 1}
+update IncrementB state = state { b = state.b + 1}
 
 view :: State -> Html Action
-view count =
+view state =
   div
     []
-    [ button [ onClick (const Increment) ] [ text "Increment" ]
-    , span [] [ text (show count) ]
-    , button [ onClick (const Decrement) ] [ text "Decrement" ]
+    [ button [ onClick (const IncrementA) ] [ text "IncrementA" ]
+    , span [] [ text (show (toNumber state.a)) ]
+    ,  span [] [ text (show (toNumber state.b)) ]
+    , button [ onClick (const IncrementB) ] [ text "IncrementB" ]
     ]
 
 main :: forall e. Eff (err :: EXCEPTION, channel :: CHANNEL | e) Unit
 main = do
+  let init = { a : 0 , b :  0 }
   app <- start
-    { initialState: 0
+    { initialState: init
     , update: fromSimple update
     , view: view
     , inputs: []
     }
 
-  renderToDOM "#app" app.html
+  renderToDOM "#container" app.html
